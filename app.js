eac3cc3dcb8217fa8a422129e3fdeb840615d1c8
= <=>
2022-05-02 23:09:58 +0700

    use bootstrap and add internal css

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const file = require("fs");
 const data = require("./json/test.json");
 
+//use internal css
+app.use(express.static("views"));
 
 //using body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,15 +23,17 @@ app.post('/send', (req, res, next) => {
         name: req.body.name,
         email: req.body.email,
     };
-    next()
-}, (req, res) => {
-        data.push(acc);
-        file.writeFile("./json/test.json", JSON.stringify(data), (err) => {
-            if (err) throw err;
-            console.log("Data written to file");
-        });
+    console.log(req.body.task)
     res.redirect('/')
-})
+    next() })
+// }, (req, res) => {
+//         data.push(acc);
+//         file.writeFile("./json/test.json", JSON.stringify(data), (err) => {
+//             if (err) throw err;
+//             console.log("Data written to file");
+//         });
+//     res.redirect('/')
+// })
 
 app.get('/api-test', (req, res) => {
     res.sendFile(__dirname + '/json/test.json')
