c81cffcd5d424856cd0260757f324a43b2bb3138
= <=>
2022-05-03 12:47:28 +0700

    CRUD (Create) new task

--- app.js
+++ app.js
@@ -3,37 +3,36 @@ const app = express()
 const bodyParser = require("body-parser");
 const file = require("fs");
 const data = require("./json/test.json");
+const time = new Date();
 
 //use internal css
 app.use(express.static("views"));
 
 //using body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //view engine ejs
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { data })
 })
 
 app.post('/send', (req, res, next) => {
     acc = {
-        name: req.body.name,
-        email: req.body.email,
+        id : time.getTime(),
+        task: req.body.task
     };
-    console.log(req.body.task)
+    next()
+}, (req, res) => {
+        data.push(acc);
+        file.writeFile("./json/test.json", JSON.stringify(data), (err) => {
+            if (err) throw err;
+            console.log("Data written to file");
+        });
     res.redirect('/')
-    next() })
-// }, (req, res) => {
-//         data.push(acc);
-//         file.writeFile("./json/test.json", JSON.stringify(data), (err) => {
-//             if (err) throw err;
-//             console.log("Data written to file");
-//         });
-//     res.redirect('/')
-// })
+})
 
 app.get('/api-test', (req, res) => {
     res.sendFile(__dirname + '/json/test.json')
